c2d4eb6c89cabc4180400cc2cc4951474a323c04
chenzhike110 <3180103610@zju.edu.cn>
2021-11-27 01:17:49 +0800

    【修改说明】增加了test example

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ subject to         $l \leq A_cX \leq u$
 
 ### MPC problem
 
-​		$\arg\min_{x_k,u_k}\sum_{k=0}^N(x_k-x_r)^TQ(x_k-x_r)+u_k^TRu_k$
+​		$\arg\min_{x_k,u_k}\sum_{k=0}^N(x_k-x_{ref_{k}})^TQ(x_k-x_{ref_{k}})+u_k^TRu_k$
 
 subject to  		$x_{k+1}=Ax_k+Bu_k$
 
@@ -19,6 +19,28 @@ subject to  		$x_{k+1}=Ax_k+Bu_k$
 ​							$u_{min}\leq u_k \leq u_{max}$
 
 ​							$x_0=x_0$
+
+### MPC to QP
+
+Hessian matrix P is equal to
+
+$P = diag(Q, Q,\ ...,Q,R,\ ...,R)$
+
+Gradient vector q is equal to
+
+$q=[-Qx_{ref_{0}}, -Qx_{ref_{1}}, \ ..., -Qx_{ref_{N}}, 0, \ ...,0]^T$
+
+Linear constraint matrix A is equal to
+
+![linear_matrix](./images/linear_matrix.png)
+
+Upper and lower bound are equal to
+
+![constaint](/home/czk119/Desktop/ZJUDancer/Eigen-quadratic-programming/images/constaint.png)
+
+### Test Example
+
+$ \min \sum_{k=0}^N \frac{1}{2}(X-X_{Ref})Q(X-X_{Ref})^T+\frac{1}{2}URU^T  \\st. X_{Ref}=[0, 0, 0.2, 0.4, 0.6, 0.8, 1, 1]  \\ X_{k+1} = \left[ \begin{matrix} 1 & \Delta t & \Delta t^2 \\ 0 & 1 & \Delta t  \\ 0 & 0 & 1\end{matrix} \right] X_k + \left[ \begin{matrix} 0 \\ 0 \\ 1 \end{matrix} \right]U_k \\ Q=diag(1, 1, 1) \\ R = 10\\X_0=0 \\ -1.0 \leq U \leq 1.0 \\ N=8, \Delta t=0.1$
 
 ### OSQP
 
--- main.cpp
+++ main.cpp
@@ -2,8 +2,78 @@
 #include <Eigen/Dense>
 #include <iostream>
 
+void SimpleSystem()
+{
+    double dt = 0.1;
+    double u_min = -1.0;
+    double u_max = 1.0;
+    Eigen::Vector3d x0{0, 0, 0};
+    const int windows = 8;
+    Eigen::Matrix<double, 8, 3> X_ref;
+    X_ref << 0., 0., 0.2, 0.4, 0.6, 0.8, 1., 1.,
+             0., 0., 2.0, 2.0, 2.0, 2.0, 0., 0.,
+             0., 20., 0.,  0.,  0.,  0.,-20.,0.; 
+    std::cout << "X_ref: \n" << X_ref << std::endl;
+    Eigen::Matrix3d A, Q;
+    A << 1,   dt, dt*dt,
+         0,    1,    dt,
+         0,    0,     1;
+
+    Q = Eigen::Matrix3d::Identity();
+    double R = 1.0;
+    Eigen::Vector3d B;
+    B << 0, 0, 1; 
+
+    Eigen::MatrixXd hessian, gradient, constaints, lowerBound, upperBound;
+    
+    // set hessian
+    hessian.resize(3 * (windows + 1) + windows, 3 * (windows + 1) + windows);
+    gradient.resize(3 * (windows + 1) + windows, 3 * (windows + 1) + windows);
+    for(int i = 1; i <= windows + 1; i++)
+    {
+        hessian.block<3, 3>(3*(i - 1), 3*(i - 1)) = Q;
+        hessian.block<1, 1>(3*windows + i - 1, 3*windows + i - 1) = R;
+        gradient.block<3, 1>(3*(i - 1), 0) = Q * X_ref.block<1, 3>()
+    }
+
+
+    // set containts and bound
+    constaints.resize(3 * (windows + 1) + windows, 3 * (windows + 1) + windows);
+    lowerBound.resize(3 * (windows + 1) + windows, 1);
+    upperBound.resize(3 * (windows + 1) + windows, 1);
+    lowerBound.setZero();
+    upperBound.setZero();
+    lowerBound.block<3, 1>(0, 0) = -x0;
+    upperBound.block<3, 1>(0, 0) = -x0; 
+    constaints.block<3 * (windows + 1), 3 * (windows + 1)>(0, 0) = -Eigen::Matrix<double, 3 * (windows + 1), 3 * (windows + 1)>::Identity();
+    constaints.block<3 * (windows + 1), windows>(0, 3 * (windows + 1)).setZero();
+    constaints.block<windows, windows>(3 * (windows + 1), 3 * (windows + 1)) = Eigen::Matrix<double, windows, windows>::Identity();
+    for(int i = 1; i <= windows; i++)
+    {
+        constaints.block<3, 3>(3 * i, 3*(i - 1)) = A;
+        constaints.block<3, 1>(3 * i, 3 * (windows + 1) + (i - 1)) = B;
+        lowerBound(3 * (windows + 1) + i, 0) = u_min;
+        upperBound(3 * (windows + 1) + i, 0) = u_max;
+    }
+    
+    // debug 
+    std::cout << "constaints: \n" << constaints << std::endl;
+    std::cout << "uppercontaint: \n" << upperBound << std::endl;
+    std::cout << "lowercontaint: \n" << lowerBound << std::endl;
+    OsqpEigen::Solver solver;
+    // settings
+    solver.settings()->setWarmStart(true);
+    
+    // set initial data of the QP solver
+    solver.data()->setNumberOfVariables(3 * (windows + 1) + (windows + 1));
+    // solver.data()->
+
+
+}
+
 int main(int argc, char** argv)
 {
-    OsqpEigen::Solver solver;
+
+    SimpleSystem();
     return 0;
 }
